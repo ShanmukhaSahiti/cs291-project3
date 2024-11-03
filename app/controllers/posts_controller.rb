5c67fd870986196a5fe1aad7c9b397bb82ee2d16
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-11-03 14:03:10 -0800

    user validation before actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_author, only: [ :edit, :update, :destroy ]
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -20,13 +21,11 @@ class PostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -35,14 +34,23 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to posts_path, status: :see_other
   end
 
   private
-    def post_params
-      params.require(:post).permit(:body, :author)
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def require_author
+    unless @post.author == current_user.username
+      redirect_to posts_path, alert: "You are not authorized to perform this action."
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:body, :author)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :body, presence: true, length: { minimum: 10, maximum: 250 }
   validates :author, presence: true
