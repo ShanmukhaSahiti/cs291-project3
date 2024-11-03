860930e4525e8934118c9a9d194e5b158e2d88ab
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-11-03 12:25:55 -0800

    login and logout routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [ :new, :create, :destroy ]
+  def new
+    # Display the login form
+  end
+
+  def create
+    # Find or create the user based on the username
+    user = User.find_or_create_by(username: params[:username])
+
+    # Store the user's ID in the session to keep them logged in
+    session[:user_id] = user.id
+
+    # Redirect to the root path
+    redirect_to root_path, notice: "Logged in as #{user.username}"
+  end
+
+  def destroy
+    # Log out the user by clearing the session
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out successfully"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_03_185446) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_03_200557) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.string "commenter"
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_03_185446) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
