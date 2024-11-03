e684cd7f8584c290974b86ae03f3d3a701870866
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-11-03 14:49:03 -0800

    fixed association between user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
+  belongs_to :user
 
   validates :body, presence: true, length: { minimum: 10, maximum: 250 }
   validates :author, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
-  belongs_to :user
   has_many :posts, dependent: :destroy
 end
