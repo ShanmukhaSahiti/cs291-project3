2f4d45b533b633c6b22e142e173f478848d24732
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-11-03 19:40:25 -0800

    time format and content validation for elections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
     # Log out the user by clearing the session
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out successfully"
+    redirect_to login_path, notice: "Logged out successfully"
   end
 end
