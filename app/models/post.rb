00f2650a8a31ed32a5c50d429530c7096b3d2a5c
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-11-03 22:59:52 -0800

    regex text for content validatiopn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   # belongs_to :user
 
-  validates :body, presence: true, length: { minimum: 10, maximum: 250 }, format: { without: /trump|harris|donald trump|kamala harris/i, message: ": Nothing related to elections please!" }
+  validates :body, presence: true, length: { minimum: 10, maximum: 250 }, format: { without: /trump|harris|donaldtrump|kamalaharris/i, message: ": Nothing related to elections please!" }
   validates :author, presence: true
 end
